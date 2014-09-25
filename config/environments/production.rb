4c7ae2656bbbd3ae0febf70c02c5d7322d9fbfaf
Ryan <hiphubryan@gmail.com>
2014-09-25 17:27:50 +0800

    Fix: hide key in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,10 +86,10 @@ Rails.application.configure do
 
     config.fog_credentials = {
       :provider               => 'AWS',                        # required
-      :aws_access_key_id      => 'AKIAJFK2O6UVG3SCIDUQ',                        # required
-      :aws_secret_access_key  => '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'                        # required   # required
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']                        # required
     }
-    config.fog_directory  = 'pinghousev2'                    # required
+    config.fog_directory  = ENV['AWS_BUCKET']                     # required
     #config.fog_public     = false                                   # optional, defaults to true
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
   end
