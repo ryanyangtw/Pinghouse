5d1c955f7b8ed1d9adefed252e5b9b26eda5140d
Ryan <hiphubryan@gmail.com>
2014-09-25 16:59:54 +0800

    Modify: put key into heroku env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,16 +82,15 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   CarrierWave.configure do |config|
-    
     config.storage :fog
 
-    #config.fog_credentials = {
-    #  :provider               => 'AWS',                        # required
-    #  :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
-    #  :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']                        # required
-    #}
-    #config.fog_directory  = ENV['AWS_BUCKET']                     # required
-    ##config.fog_public     = false                                   # optional, defaults to true
-    #config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+    config.fog_credentials = {
+      :provider               => 'AWS',                        # required
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']                        # required
+    }
+    config.fog_directory  = ENV['AWS_BUCKET']                     # required
+    #config.fog_public     = false                                   # optional, defaults to true
+    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
   end
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,10 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
-    :aws_secret_access_key  => 'Yfa6OeG85W3+gIw82SahWlpJJFVgt5e6tD3vVbSc'                       # required
-  }
-  config.fog_directory  = ENV['AWS_BUCKET']                     # required
-  #config.fog_public     = false                                   # optional, defaults to true
-  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
-end
+#CarrierWave.configure do |config|
+#  config.fog_credentials = {
+#    :provider               => 'AWS',                        # required
+#    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
+#    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']                        # required
+#  }
+#  config.fog_directory  = ENV['AWS_BUCKET']                     # required
+#  #config.fog_public     = false                                   # optional, defaults to true
+#  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+#end
