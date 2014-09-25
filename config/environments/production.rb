c1da685ebc327ed33cba77542d0fc4b175811f01
Ryan <hiphubryan@gmail.com>
2014-09-25 16:51:56 +0800

    Fix: move carrierwave config to initializer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,13 +85,13 @@ Rails.application.configure do
     
     config.storage :fog
 
-    config.fog_credentials = {
-      :provider               => 'AWS',                        # required
-      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
-      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']                        # required
-    }
-    config.fog_directory  = ENV['AWS_BUCKET']                     # required
-    #config.fog_public     = false                                   # optional, defaults to true
-    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+    #config.fog_credentials = {
+    #  :provider               => 'AWS',                        # required
+    #  :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
+    #  :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']                        # required
+    #}
+    #config.fog_directory  = ENV['AWS_BUCKET']                     # required
+    ##config.fog_public     = false                                   # optional, defaults to true
+    #config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
   end
 end
