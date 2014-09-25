f6051fc9e7fb55744c1f86735b8bcd5eb9f9ad5b
Ryan <hiphubryan@gmail.com>
2014-09-25 17:50:41 +0800

    ADD: add heroku deflator to gzip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,17 +20,25 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  #config.serve_static_assets = false
+  
+  #add for heroku-deflator
+  config.serve_static_assets=true
+  config.assets.compress=true
+  config.assets.compile = true
+  config.assets.digest=true
+  config.static_cache_control="public, max-age=31536000"
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  #config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
-  config.assets.digest = true
+  #config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
